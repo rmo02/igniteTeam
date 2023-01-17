5bbcad390320f2b0c525d2444e846f46c8160a0b
rmo02 <ramoon.maia23@gmail.com>
2023-01-17 17:26:28 -0300

    criando tema

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Title } from './styles';
+
+
+export function Groups() {
+  return (
+    <Container>
+        <Title>Groups</Title>
+    </Container>
+  )
+}
+
+  
