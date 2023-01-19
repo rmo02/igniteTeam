85dd172108b89166ddd48d936747898909a9445f
rmo02 <ramoon.maia23@gmail.com>
2023-01-19 14:27:23 -0300

    iniciando rotas

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,19 +1,68 @@
+import { FlatList } from "react-native";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Hightlight } from "@components/Hightlight";
-import { Container } from "./styles";
+import { Input } from "@components/Input";
+import { Container, Form } from "./styles";
+import { useState } from "react";
+import { HeaderList, NumbersOfPlayers } from "@components/Filter/styles";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
+export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPLayers] = useState([]);
+  return (
+    <Container>
+      <Header showBackButton />
+      <Hightlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
 
-export function Players(){
-    return (
-        <Container>
-            <Header showBackButton/>
-            <Hightlight 
-            title="Nome da turma"
-            subtitle="adicione a galera e separe os times"
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
             />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
 
-            <ButtonIcon />
-        </Container>
-    );
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+
+      <Button 
+      title="Remover Turma"
+      type="SECONDARY"
+      />
+    </Container>
+  );
 }
